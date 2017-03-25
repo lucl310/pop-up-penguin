5de068d67ae28ad4d641e1f8ff47043e142ed9c6
lucl310 <lucthehuman21@gmail.com>
2017-03-25 12:02:46 -1000

    added tons of quotes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 function roar(){
      alert("Yaaaarrrr!");
 }
+var music = new Audio('https://www.youtube.com/watch?v=I4ZC_Y3Dv1w')
+var music2 = new Audio('https://www.youtube.com/watch?v=e6vhM7waF3k')
+var music3 = new Audio('https://www.youtube.com/watch?v=WWWldG1W24g')
+var music4 = new Audio('https://www.youtube.com/watch?v=eToCsR8u2FA')
+var music5 = new Audio('https://www.youtube.com/watch?v=4heVhS13bdY')
+var music6 = new Audio('https://www.youtube.com/watch?v=Q73gUUr8Zlw')
+var music7 = new Audio('https://www.youtube.com/watch?v=iAPcgY698Jw')
+var music8 = new Audio('https://www.youtube.com/watch?v=CT1SGp-ZDmw')
+
+function play() {
+	audio.play1()
+}
+function play1() {
+	music.play2()
+}
+function play2() {
+	music.play3()
+}
+function play3() {
+	music.play4()
+}
+function play4() {
+	music.play5()
+}
+function play5() {
+	music.play6()
+}
+function play6() {
+	music.play7()
+}
+function play7() {
+	music.play8()
+}
+function play8() {
+	music.play9()
+}
